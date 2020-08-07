746daa5a507467b9cfb943f2b5a535048408fb67
Tomoyuki Sakurai <y@trombik.org>
2020-08-08 08:14:35 +0900

    bugfix: fix ci, the tests should pass in virtualbox

--- spec/serverspec/shared_examples/hostname.rb
+++ spec/serverspec/shared_examples/hostname.rb
@@ -7,19 +7,19 @@ shared_examples "a host with a valid hostname" do
     let(:command_output_as_json) { MultiJson.load(subject.stdout.gsub("localhost | SUCCESS =>", "")) }
 
     it "exit_status" do
-      pending "ansible in not available on the target"
+      pending "ansible in not available on the target" if test_environment == "prod"
       expect(subject.exit_status).to eq 0
     end
     it "outputs a valid JSON" do
-      pending "ansible in not available on the target"
+      pending "ansible in not available on the target" if test_environment == "prod"
       expect { command_output_as_json }.not_to raise_error
     end
     it "contains correct ansible_fqdn" do
-      pending "ansible in not available on the target"
+      pending "ansible in not available on the target" if test_environment == "prod"
       expect(command_output_as_json).to include("ansible_facts" => include("ansible_fqdn" => ENV["TARGET_HOST"]))
     end
     it "contains correct ansible_hostname" do
-      pending "ansible in not available on the target"
+      pending "ansible in not available on the target" if test_environment == "prod"
       expect(command_output_as_json).to include("ansible_facts" => include("ansible_hostname" => ENV["TARGET_HOST"].split(".").first))
     end
   end
