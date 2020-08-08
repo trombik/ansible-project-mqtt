348cddb880654a18b3b96a0372c03e9d6c46f864
Tomoyuki Sakurai <y@trombik.org>
2020-08-08 16:01:53 +0900

    feature: enable TLS
    also, fix incorrect monit check for mosquitto.

--- spec/serverspec/mqtt/default_spec.rb
+++ spec/serverspec/mqtt/default_spec.rb
@@ -3,9 +3,6 @@
 require_relative "../spec_helper"
 require "net/http"
 
-ports = [
-  1883 # mosquitto
-]
 services = %w[
   monit
   mosquitto
@@ -23,12 +20,6 @@ services.each do |s|
   end
 end
 
-ports.each do |p|
-  describe port p do
-    it { should be_listening }
-  end
-end
-
 describe command "monit status mosquitto" do
   # XXX use retry here because monit and mosquitto need some time to start up
   it "says monit is monitroing mosquitto", retry: 3, retry_wait: 10 do
@@ -37,16 +28,6 @@ describe command "monit status mosquitto" do
     expect(command.exit_status).to eq 0
     expect(command.stderr).to eq ""
     expect(command.stdout).to match(/monitoring mode\s+active/)
-  end
-end
-
-topic = "$SYS/broker/clients/connected"
-describe "mosquitto_sub" do
-  it "returns #{topic}", retry: 3, retry_wait: 10 do
-    command = command("mosquitto_sub -h 127.0.0.1 -C 1 -t #{Shellwords.escape(topic)}")
-
-    expect(command.exit_status).to eq 0
-    expect(command.stderr).to eq ""
-    expect(command.stdout).to match(/^\d+$/)
+    expect(command.stdout).to match(/monitoring status\s+Monitored/)
   end
 end
