dfd3bd738eb170d726aee4972c55c367abf9f0d0
Tomoyuki Sakurai <y@trombik.org>
2020-08-08 16:01:53 +0900

    feature: enable TLS
    also, fix incorrect monit check for mosquitto.

---
+++ spec/serverspec/mqtt/tls_spec.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+require_relative "../spec_helper"
+
+topic = "$SYS/broker/clients/connected"
+ports = [8883]
+ca_cert_file = "/etc/mosquitto/certs/ca.pem"
+flags = case test_environment
+        when "virtualbox"
+          "--insecure"
+        else
+          ""
+        end
+
+ports.each do |p|
+  describe port p do
+    it { should be_listening }
+  end
+end
+
+describe file ca_cert_file do
+  it { should exist }
+  it { should be_file }
+end
+
+describe "mosquitto_sub" do
+  it "returns #{topic}", retry: 3, retry_wait: 10 do
+    command = command("mosquitto_sub -C 1 -t #{Shellwords.escape(topic)} --cafile #{Shellwords.escape(ca_cert_file) } #{Shellwords.escape(flags)}")
+
+    expect(command.exit_status).to eq 0
+    expect(command.stderr).to eq ""
+    expect(command.stdout).to match(/^\d+$/)
+  end
+end
