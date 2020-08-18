79d4ba861b74aa95ef30460338c5ef7337e0973d
Tomoyuki Sakurai <y@trombik.org>
2020-08-18 12:57:23 +0900

    bugfix: lint

--- spec/serverspec/mqtt/200_tls_spec.rb
+++ spec/serverspec/mqtt/200_tls_spec.rb
@@ -5,12 +5,7 @@ require_relative "../spec_helper"
 topic = "$SYS/broker/clients/connected"
 ports = [8883]
 ca_cert_file = "/etc/mosquitto/certs/ca.pem"
-flags = case test_environment
-        when "virtualbox"
-          "--insecure"
-        else
-          "--insecure"
-        end
+flags = "--insecure"
 
 ports.each do |p|
   describe port p do
@@ -25,7 +20,7 @@ end
 
 describe "mosquitto_sub" do
   it "returns #{topic}", retry: 3, retry_wait: 10 do
-    command = command("mosquitto_sub -C 1 -t #{Shellwords.escape(topic)} --cafile #{Shellwords.escape(ca_cert_file)} #{Shellwords.escape(flags) if flags.length > 0}")
+    command = command("mosquitto_sub -C 1 -t #{Shellwords.escape(topic)} --cafile #{Shellwords.escape(ca_cert_file)} #{Shellwords.escape(flags) unless flags.empty?}")
 
     expect(command.exit_status).to eq 0
     expect(command.stderr).to eq ""
